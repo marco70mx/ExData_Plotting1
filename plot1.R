9fd4a961405242a60b7a0b10774acfa2fd57d35f
marco70mx <75866820+marco70mx@users.noreply.github.com>
2022-02-01 19:50:12 -0600

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -26,6 +26,6 @@ hpower$dateTime <- as.POSIXct(dateTime)
 hist(hpower$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
 
 ##Crear el archivo png
-#dev.copy(png,"plot1.png", width=480, height=480)
-#dev.off()
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
 
